49836acdb8fb67e97720c9f6a45eb0e86a435472
K.X <xuxiang@solantec.com.cn>
2024-04-08 21:52:09 +0800

    add cache annotion

--- macro/src/main/scala/dev/ironduck/macro/Cache.scala
+++ macro/src/main/scala/dev/ironduck/macro/Cache.scala
@@ -1,37 +1,22 @@
 package dev.ironduck.`macro`
 
-import scala.collection.*
-import scala.reflect.ClassTag
+import scala.collection.mutable
 import scala.quoted.Quotes
 import scala.quoted.Expr
+import scala.reflect.ClassTag
 
 trait Cache[K, V]:
   def put(key: K, vale: V): Option[V]
   def get(key: K): Option[V]
+
+trait CacheFactory:
+  def apply[K: ClassTag, V: ClassTag](name: String): Cache[K, V]
 
 class MapCache[K, V] extends Cache[K, V]:
   private val map = mutable.Map.empty[K, V]
   override def put(key: K, value: V): Option[V] = map.put(key, value)
 
   override def get(key: K): Option[V] = map.get(key)
-
-class LogMapCache[K, V](n: String)(using ktag: ClassTag[K], vtag: ClassTag[V]) extends Cache[K, V]:
-  private val name = s"LogMapCache[$ktag, $vtag](name=\"$n\")"
-  private val map = mutable.Map.empty[K, V]
-
-  LogMapCache.log.append(s"Create $name")
-
-  override def put(key: K, value: V): Option[V] =
-    val result = map.put(key, value)
-    LogMapCache.log.append(s"$name.put($key, $value)=$result")
-    result
-  override def get(key: K): Option[V] =
-    val result = map.get(key)
-    LogMapCache.log.append(s"$name.get($key)=$result")
-    result
-
-object LogMapCache:
-  val log = mutable.Buffer.empty[String]
 
 @experimental
 class cached extends MacroAnnotation:
@@ -52,23 +37,28 @@ class cached extends MacroAnnotation:
 
         val paramTupleTypeRepr = paramTuple match
           case '{ $p: parTupleType } =>
-            val t = TypeRepr.of[parTupleType].asInstanceOf[AppliedType]
-            val args = t.typeArgs.map(_.widen)
-            t.tycon.appliedTo(args)
+            TypeRepr.of[parTupleType] match
+              case t: AppliedType =>
+                val args = t.typeArgs.map(_.widen)
+                t.tycon.appliedTo(args)
+              case _: TypeRepr =>
+                TypeRepr.of[EmptyTuple]
 
         (paramTupleTypeRepr.asType, returnType.tpe.asType) match
           case ('[paramTupleType], '[rhsType]) =>
             val cacheName = Symbol.freshName(name + "Cache")
             val cacheType = TypeRepr.of[Cache[paramTupleType, rhsType]]
-            val ktag = Expr.summon[ClassTag[paramTupleType]].get
-            val vtag = Expr.summon[ClassTag[rhsType]].get
 
-            val cacheSummoned = Expr.summon[Cache[paramTupleType, rhsType]]
-            println(cacheSummoned.map(_.asTerm.show(using Printer.TreeCode)))
-
-            val cacheRhs = '{
-              new LogMapCache[paramTupleType, rhsType](${ Expr(cacheName) })(using $ktag, $vtag)
-            }.asTerm
+            val cacheRhs = Expr.summon[CacheFactory] match
+              case Some(cacheFactory) =>
+                val ktag = Expr.summon[ClassTag[paramTupleType]].get
+                val vtag = Expr.summon[ClassTag[rhsType]].get
+                '{
+                  $cacheFactory
+                    .apply[paramTupleType, rhsType](${ Expr(cacheName) })(using $ktag, $vtag)
+                }.asTerm
+              case None =>
+                '{ MapCache[paramTupleType, rhsType] }.asTerm
 
             val cacheSymbol = Symbol.newVal(
               tree.symbol.owner,
@@ -95,8 +85,6 @@ class cached extends MacroAnnotation:
             val newRhs = buildNewRhs(using tree.symbol.asQuotes).asTerm
             val expandedMethod = DefDef.copy(tree)(name, params, returnType, Some(newRhs))
 
-            println(cache.show(using Printer.TreeCode))
-            println(expandedMethod.show(using Printer.TreeCode))
             List(cache, expandedMethod)
       case _ =>
         report.error("Annottee must be a method")
--- macro/src/test/scala/dev/ironduck/macro/CacheSuite.scala
+++ macro/src/test/scala/dev/ironduck/macro/CacheSuite.scala
@@ -1,5 +1,26 @@
 package dev.ironduck.`macro`
 
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+class LogMapCache[K, V](n: String)(using ktag: ClassTag[K], vtag: ClassTag[V]) extends Cache[K, V]:
+  private val name = s"LogMapCache[$ktag, $vtag](name=\"$n\")"
+  private val map = mutable.Map.empty[K, V]
+
+  LogMapCache.log.append(s"Create $name")
+
+  override def put(key: K, value: V): Option[V] =
+    val result = map.put(key, value)
+    LogMapCache.log.append(s"$name.put($key, $value)=$result")
+    result
+  override def get(key: K): Option[V] =
+    val result = map.get(key)
+    LogMapCache.log.append(s"$name.get($key)=$result")
+    result
+
+object LogMapCache:
+  val log = mutable.Buffer.empty[String]
+
 @experimental
 class CacheSuite extends munit.FunSuite:
   test("LogMapCache should work"):
@@ -24,13 +45,80 @@ class CacheSuite extends munit.FunSuite:
 
   import CacheSuite.given
 
-  @cached
-  def f(x: Int, y: Int): Int = x * y
+  test("@cache should work on no-arity function"):
+    @cached
+    def noArity() = 255 * 3
 
-  test("cache annotation should work"):
-    assertEquals(f(1, 2), 2)
-    assertEquals(f(1, 2), 2)
+    assertEquals(noArity(), 255 * 3)
+    assertEquals(noArity(), 255 * 3)
+    assert(
+      LogMapCache.log.containsSlice(
+        List(
+          "Create LogMapCache[scala.Tuple$package$EmptyTuple$, Int](name=\"noArityCache$macro$1\")",
+          "LogMapCache[scala.Tuple$package$EmptyTuple$, Int](name=\"noArityCache$macro$1\").get(())=None",
+          "LogMapCache[scala.Tuple$package$EmptyTuple$, Int](name=\"noArityCache$macro$1\").put((), 765)=None",
+          "LogMapCache[scala.Tuple$package$EmptyTuple$, Int](name=\"noArityCache$macro$1\").get(())=Some(765)",
+        )
+      )
+    )
+
+  test("@cache should work on one-arity function"):
+    @cached
+    def oneArity(x: Int): Int = x * 2
+
+    assertEquals(oneArity(2), 4)
+    assertEquals(oneArity(2), 4)
+    assert(
+      LogMapCache.log.containsSlice(
+        List(
+          "Create LogMapCache[scala.Tuple1, Int](name=\"oneArityCache$macro$1\")",
+          "LogMapCache[scala.Tuple1, Int](name=\"oneArityCache$macro$1\").get((2))=None",
+          "LogMapCache[scala.Tuple1, Int](name=\"oneArityCache$macro$1\").put((2), 4)=None",
+          "LogMapCache[scala.Tuple1, Int](name=\"oneArityCache$macro$1\").get((2))=Some(4)",
+        )
+      )
+    )
+
+  test("@cache should work on two-arity function"):
+    @cached
+    def twoArity(x: Int, y: Int): Int = x * y
+
+    assertEquals(twoArity(1, 2), 2)
+    assertEquals(twoArity(1, 2), 2)
+    assert(
+      LogMapCache.log.containsSlice(
+        List(
+          "Create LogMapCache[scala.Tuple2, Int](name=\"twoArityCache$macro$1\")",
+          "LogMapCache[scala.Tuple2, Int](name=\"twoArityCache$macro$1\").get((1,2))=None",
+          "LogMapCache[scala.Tuple2, Int](name=\"twoArityCache$macro$1\").put((1,2), 2)=None",
+          "LogMapCache[scala.Tuple2, Int](name=\"twoArityCache$macro$1\").get((1,2))=Some(2)",
+        )
+      )
+    )
+
+  test("@cache should work on three-arity function"):
+    @cached
+    def threeArity(
+      x: Int,
+      y: Int,
+      z: Int,
+    ) = x + y + z
+
+    assertEquals(threeArity(3, 2, 1), 6)
+    assertEquals(threeArity(3, 2, 1), 6)
+    assert(
+      LogMapCache.log.containsSlice(
+        List(
+          "Create LogMapCache[scala.Tuple3, Int](name=\"threeArityCache$macro$1\")",
+          "LogMapCache[scala.Tuple3, Int](name=\"threeArityCache$macro$1\").get((3,2,1))=None",
+          "LogMapCache[scala.Tuple3, Int](name=\"threeArityCache$macro$1\").put((3,2,1), 6)=None",
+          "LogMapCache[scala.Tuple3, Int](name=\"threeArityCache$macro$1\").get((3,2,1))=Some(6)",
+        )
+      )
+    )
     LogMapCache.log.foreach(println)
 
 object CacheSuite:
-  given Cache[(Int, Int), Int] = new MapCache[(Int, Int), Int]
+  given CacheFactory with
+    def apply[K: ClassTag, V: ClassTag](name: String): Cache[K, V] =
+      LogMapCache(name)
